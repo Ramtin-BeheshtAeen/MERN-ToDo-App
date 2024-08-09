7f1365d073a2bffeb5ab963f84f5664c43736b7e
Ramtin_BeheshtAeen_Laptop <Ramtinba145822@gmail.com>
2024-08-09 19:08:02 +0200

    add calender

--- client/src/components/Form/DatePicker.jsx
+++ client/src/components/Form/DatePicker.jsx
@@ -1,24 +1,32 @@
+// src/components/DateTimePicker.js
 import React, { useState } from 'react';
 import { LocalizationProvider, DatePicker, TimePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TextField } from '@mui/material';
 import dayjs from 'dayjs';
 
-const DatePickerComponent = () => {
+function DateTimePicker({ selectedDate, selectedTime, onDateChange, onTimeChange }) {
+  const [selectedDate, setSelectedDate] = useState(dayjs());
+  const [selectedTime, setSelectedTime] = useState(dayjs());
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <div>
-      <DatePicker
-        label="Helper text example"
-        slotProps={{
-          textField: {
-            helperText: "MM/DD/YYYY",
-          },
-        }}
-      />
-    </div>
+      <div className='date-picker'>
+        <DatePicker
+          label="Select Date"
+          value={selectedDate}
+          onChange={onDateChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        <TimePicker
+          label="Select Time"
+          value={selectedTime}
+          onChange={(newValue) => setSelectedTime(newValue)}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </div>
     </LocalizationProvider>
   );
-};
+}
 
-export default DatePickerComponent;
+export default DateTimePicker;
