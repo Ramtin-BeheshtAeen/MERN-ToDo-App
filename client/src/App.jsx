4306602aebc5f70135132bde37dd7185e3a4844c
Ramtin_BeheshtAeen_Laptop <Ramtinba145822@gmail.com>
2024-08-13 23:39:07 +0200

    add Authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,56 @@
-import { useEffect, useState, React } from 'react'
-import ListHeader from './components/ListHeader'
-import ListItem from './components/ListItem'
+import { useEffect, useState, React } from "react";
+import ListHeader from "./components/ListHeader";
+import ListItem from "./components/ListItem";
+import Auth from "./components/Auth";
 
 function App() {
-  
-  const userId = "66b892e2bfcdfca87b50d64e"
-  const [task, setTask] = useState(null)
-  
+  const userId = "66b892e2bfcdfca87b50d64e";
+  const [task, setTask] = useState(null);
 
-  async function getData(){
-    try{
-      const response = await fetch(`http://localhost:8000/todo/${userId}`)
-      const json = await response.json()
-      setTask(json)
+  const authToken = true;
 
-    } catch(err) {
-      console.log(err)
+  async function getData() {
+    try {
+      const response = await fetch(`http://localhost:8000/todo/${userId}`);
+      const json = await response.json();
+      setTask(json);
+    } catch (err) {
+      console.log(err);
     }
   }
 
-  useEffect( () => getData, [])
+  useEffect(() => getData, []);
 
   //Sort Tasks By Date:
   // if they exist:
-  const sortedTasks = task?.sort( (a, b) => new Date(a.createdAt) - new Date(b.createdAt) ) 
-  console.log(sortedTasks)
-  
+  const sortedTasks = task?.sort(
+    (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+  );
+  console.log(sortedTasks);
+
   return (
-   <div className='app'>
-    <ListHeader listName={'Holiday Tick List'} userId={userId} getData={getData}/>
-    {sortedTasks?.map( (task) => <ListItem key={task._id} task={task} userId={userId} getData={getData} />)}
-   </div>
-  )
+    <div className="app">
+      {!authToken && <Auth />}
+
+      {authToken && (
+        <>
+          <ListHeader
+            listName={"Holiday Tick List"}
+            userId={userId}
+            getData={getData}
+          />
+          {sortedTasks?.map((task) => (
+            <ListItem
+              key={task._id}
+              task={task}
+              userId={userId}
+              getData={getData}
+            />
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,48 @@
+import { React, useState } from "react";
+
+const Auth = () => {
+  const [isLogIn, setIsLogIn] = useState(true);
+  const [error, setError] = useState(null);
+
+  const viewLogin = (status) => {
+    setError(null);
+    setIsLogIn(status);
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-container-box"></div>
+      <form>
+        <h2>{isLogIn ? "Please Login" : "Please Sign up"}</h2>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        {!isLogIn && <input type="password" placeholder="confirm password" />}
+        <input type="submit" className="create" />
+        <p></p>
+      </form>
+
+      <div className="auth-options"></div>
+      <button
+        onClick={() => viewLogin(false)}
+        style={{
+          backgroundColor: !isLogIn
+            ? "rgb(255, 255, 255)"
+            : "rgb(188, 188, 188)",
+        }}>
+        Sign Up
+      </button>
+
+      <button
+        onClick={() => viewLogin(true)}
+        style={{
+          backgroundColor: !isLogIn
+            ? "rgb(255, 255, 255)"
+            : "rgb(188, 188, 188)",
+        }}>
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Auth;
