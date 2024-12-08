b8cbdf9146350dac9c6233a57559257b2956a6e6
RamtinBeheshtAeen <ramtinba145822@gmail.com>
2024-12-08 12:54:25 +0100

    added Online Database to project

--- server/Db.js
+++ server/Db.js
@@ -2,18 +2,22 @@ import mongoose from "mongoose";
 
 export default function connectDB() {
   //Ethernet adapter vEthernet (WSL): 172.23.192.1
-  const url = "mongodb://172.18.160.1:27017/to_do_app";
+  // const url = "mongodb://172.25.240.1:27017/to_do_app";
+  const url = process.env.MONGODB_URI
   
   try {
     mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
   } catch (err) {
     console.error(err.message);
     process.exit(1);
   }
+
   const dbConnection = mongoose.connection;
+
   dbConnection.once("open", (_) => {
     console.log(`Database connected: ${url}`);
   });
